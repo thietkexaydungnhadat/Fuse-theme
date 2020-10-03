be67c2d0995c15cc7cac655c19f7dfc927261d2f
thietkexaydungnhadat <66399347+thietkexaydungnhadat@users.noreply.github.com>
2020-10-03 16:00:46 +0700

    setup core layout

---
+++ src/app/core/layout/components/quick-panel/quick-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-panel',
+  templateUrl: './quick-panel.component.html',
+  styleUrls: ['./quick-panel.component.scss']
+})
+export class QuickPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
